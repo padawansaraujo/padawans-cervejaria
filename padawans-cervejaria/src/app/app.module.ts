74695cb0aec3625b695f0f6544d7eeaf4c346af6
Augusto Noronha <augustonoronha@frwk.com.br>
2020-12-28 09:49:55 -0300

    header-shared

--- padawans-cervejaria/src/app/app.module.ts
+++ padawans-cervejaria/src/app/app.module.ts
@@ -1,18 +1,23 @@
+import {  FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatIconModule} from '@angular/material/icon';
+import { HeaderComponent } from './shared/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatIconModule
+    MatIconModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
