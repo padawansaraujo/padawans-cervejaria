d3362d3954570f336b0d1e688c7bdd0bb288ee43
Érika Marcia <erikamarciapereira@gmail.com>
2021-01-08 19:31:13 -0300

    Melhoria do sistema de login

--- padawans-cervejaria/src/app/services/login.service.ts
+++ padawans-cervejaria/src/app/services/login.service.ts
@@ -10,40 +10,52 @@ export class LoginService {
 
   navigation: string;
   userType: BehaviorSubject<any> = new BehaviorSubject('');
-  autenticado: boolean = false;
+  autenticados: boolean = false;
   usuario = {
-    nome: 'sadsad',
-    fotoUrl: 'asdsa'
+    nome: '0',
+    fotoUrl: '0',
+    token: '0'
   };
+
   constructor(
     public http: HttpClient, 
     private router: Router,
     private ngZone: NgZone, 
-  ) {
-
-   }
+  ) {}
+  
    login(auth2, loginElement) {
     auth2.attachClickHandler(loginElement.nativeElement, {},
       (googleUser) => {
-        const id_token = JSON.stringify(googleUser.getAuthResponse().session_state);
+        
         const perfil = googleUser.getBasicProfile();
-        //this.usuario = JSON.stringify(perfil.getImageUrl());
+        
         this.usuario.nome = perfil.getName();
         this.usuario.fotoUrl = perfil.getImageUrl();
+        this.usuario.token =  googleUser.getAuthResponse().id_token;
 
-        window.sessionStorage.setItem('nome',`${this.usuario.nome}`);
-        window.sessionStorage.setItem('foto',`${this.usuario.fotoUrl}`);
-        this.autenticado = true;
+        this.setUser();
 
         this.redirecionar();
       }, (error) => {
         console.log(error);
       });
   }
-  verificaUsuarioAutenticado(){
-    return this.autenticado;
+
+  setUser(){
+    window.sessionStorage.setItem('nome',`${this.usuario.nome}`);
+    window.sessionStorage.setItem('foto',`${this.usuario.fotoUrl}`);
+    window.sessionStorage.setItem('token',`${this.usuario.token}`);
   }
+
   redirecionar(){
     this.ngZone.run(() => this.router.navigate(["/listagens"])).then();
+  }
+
+  autenticado(){
+    let token = window.sessionStorage.getItem('token');
+    if (token != null && token != 'undefined' && token != '0'){
+      
+      return true;
+    } 
   }
 }
